5ceb9d281d4cfd2a452b0e89da76a025e9278ac1
GennadiiBukh <gennadii.bukh@gmail.com>
2023-07-29 10:50:59 +0300

    Second commit

--- get_birthday_date.py
+++ get_birthday_date.py
@@ -1,11 +1,11 @@
-from datetime import datetime, timedelta
+from datetime import datetime, date, timedelta
 
 employees = [{'name':'Іван', 'birthday':'1994-08-01'}, 
              {'name':'Олег', 'birthday':'1995-08-02'},
              {'name':'Ганна', 'birthday':'1996-07-27'},
              {'name':'Андрій', 'birthday':'1999-01-17'},
              {'name':'Олександр', 'birthday':'2000-10-09'},
-             {'name':'Сергій', 'birthday':'2003-08-02'},
+             {'name':'Сергій', 'birthday':'2003-08-05'},
              {'name':'Аліна', 'birthday':'2004-07-29'},
              {'name':'Людмила', 'birthday':'2001-07-30'}]
 
@@ -21,27 +21,27 @@ def get_birthdays_per_week(users):
     6: "неділя",
     }
 
-    current_date = datetime.now()
+    current_date = date.today()
+
+    if current_date.weekday() == 0:
+        current_date = current_date - timedelta(days=2)
+
     one_week = timedelta(days=6)
     one_week_later = current_date + one_week
-    print(f'Поздоровити іменинників від {current_date.date()} до {one_week_later.date()}\n')
 
-    to_congrat = {0:[],1:[],2:[],3:[],4:[]}
+    print(f'Поздоровити іменинників від {current_date} до {one_week_later}\n')
+
+    to_congrat = {i:[] for i in range(0,6)}
 
     for user in users:
-        birthdate = datetime.strptime(user.get('birthday'), "%Y-%m-%d") # Переводимо ДН з рядка в datetime
-        birthdate_this_year = birthdate.replace(year=current_date.year) # Встановлюємо ДН у поточному році  
+          
+        birthdate_this_year = date.fromisoformat(user['birthday']).replace(year=current_date.year) # Встановлюємо ДН у поточному році 
         day_of_week = None
-        last_Sunday = current_date.date()-timedelta(days=1)
-        last_Saturday = current_date.date()-timedelta(days=2)         
-       
-        if current_date.date() <= birthdate_this_year.date() <= one_week_later.date(): # Перевіряємо чи припадає ДН на сьогодні чи тиждень від сьогодні
+           
+        if current_date <= birthdate_this_year <= one_week_later: # Перевіряємо чи припадає ДН на сьогодні чи тиждень від сьогодні
            day_of_week = birthdate_this_year.weekday() # Отримуємо день тижня коли буде ДН
-           if day_of_week == 5 or day_of_week == 6:    # Якщо ДН у вихідний - поздоровляємо у понеділок
+           if day_of_week in [5,6]:    # Якщо ДН у вихідний - поздоровляємо у понеділок
                day_of_week = 0
-        elif current_date.weekday() == 0:  # Якщо сьогодні понеділок - треба поздоровити сьогодні іменинників з минулих вихідних
-            if (birthdate_this_year.date() == last_Sunday) or (birthdate_this_year.date() == last_Saturday):
-                to_congrat[0].append(user['name'])
         else:
             continue
         for day, name in to_congrat.items(): # Формуємо списки поздоровлень по днях
@@ -49,10 +49,8 @@ def get_birthdays_per_week(users):
                 to_congrat[day].append(user['name'])
 
     for day, names in to_congrat.items(): # Друкуємо списки на поздоровлення по наступних днях
-        if names != []:
+        if names:
             names_str = ', '.join(names)
             print(f'{days_name[day]:<10}: {names_str}')
 
 get_birthdays_per_week(employees)
-
-
